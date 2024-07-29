ef0ff30b670045a08271c079be4501ea4964f61d
Dawei Pang <138844843+DaweiPang32@users.noreply.github.com>
2024-07-29 15:26:39 -0700

    Add files via upload

--- road/scatter.py
+++ road/scatter.py
@@ -4,9 +4,15 @@ import json
 import yaml
 from sklearn.linear_model import LinearRegression
 
-def compute_and_save_errors(est_df, obs_df, chosen_timeperiod, combined_df_cols, classification_col):
+
+def compute_and_save_errors(
+        est_df,
+        obs_df,
+        chosen_timeperiod,
+        combined_df_cols,
+        classification_col):
     # Compute errors
-    times = ['Daily','AM', 'MD', 'PM', 'EV', 'EA']
+    times = ['Daily', 'AM', 'MD', 'PM', 'EV', 'EA']
     error = {col: est_df[col] - obs_df[col] for col in times}
     error_df = pd.DataFrame(error)
     error_squared_df = error_df.pow(2)
@@ -14,31 +20,51 @@ def compute_and_save_errors(est_df, obs_df, chosen_timeperiod, combined_df_cols,
     error_percent_df = error_percent_df.apply(lambda x: x * 100, axis=1)
 
     # Combine dataframes
-    combined_df = pd.concat([est_df, obs_df[times], error_df, error_squared_df, error_percent_df], axis=1)
+    combined_df = pd.concat(
+        [est_df, obs_df[times], error_df, error_squared_df, error_percent_df], axis=1)
     Select_time_period_df = combined_df.filter(like=chosen_timeperiod)
     specific_columns_combined_df = combined_df[combined_df_cols]
-    Select_time_period_loc_df = pd.concat([specific_columns_combined_df, Select_time_period_df], axis=1)
+    Select_time_period_loc_df = pd.concat(
+        [specific_columns_combined_df, Select_time_period_df], axis=1)
 
     # Set column names
-    calculation_cols = ['Estimated Volume', 'Observed Volume', 'Errors', 'Squared Errors', 'Percent Errors']
+    calculation_cols = [
+        'Estimated Volume',
+        'Observed Volume',
+        'Errors',
+        'Squared Errors',
+        'Percent Errors']
     Select_time_period_loc_df.columns = combined_df_cols + calculation_cols
 
     # Drop duplicates
-    Select_time_period_loc_df = Select_time_period_loc_df.drop_duplicates(subset=['A', 'B'], keep='first')
+    Select_time_period_loc_df = Select_time_period_loc_df.drop_duplicates(
+        subset=['A', 'B'], keep='first')
 
     # Save to CSV
     file_name = f"{chosen_timeperiod}_Scatter_data.csv"
     Select_time_period_loc_df.to_csv(file_name, index=False)
 
     # Get classification column types
-    classification_col_types = Select_time_period_loc_df[classification_col].unique()
-    
-    return Select_time_period_loc_df, classification_col_types,file_name
+    classification_col_types = Select_time_period_loc_df[classification_col].unique(
+    )
+
+    return Select_time_period_loc_df, classification_col_types, file_name
+
 
 def sanitize_filename(filename):
     return filename.replace('/', '_').replace('\\', '_')
 
-def generate_vega_lite_json_EST(csv_file, classification_col, classification_col_types, x_field, y_field, fields, nominal_fields, name, include_all_data = False):
+
+def generate_vega_lite_json_EST(
+        csv_file,
+        classification_col,
+        classification_col_types,
+        x_field,
+        y_field,
+        fields,
+        nominal_fields,
+        name,
+        include_all_data=False):
     df = pd.read_csv(csv_file)
     if not include_all_data:
         df = df[df[classification_col] == classification_col_types]
@@ -54,11 +80,13 @@ def generate_vega_lite_json_EST(csv_file, classification_col, classification_col
     diagonal_df = pd.DataFrame({
         x_field: [df[x_field].min(), df[x_field].max()],
         y_field: [df[x_field].min(), df[x_field].max()]  # Diagonal line: y = x
-    }) 
+    })
     equation_text = f"y = {m:.2f}x + {b:.2f}"
     max_value = max(df[x_field].max(), df[y_field].max())
-    tooltip_config = [{"field": field, "type": "nominal" if field in nominal_fields else "quantitative", "title": field} for field in fields]
-    
+    tooltip_config = [{"field": field,
+                       "type": "nominal" if field in nominal_fields else "quantitative",
+                       "title": field} for field in fields]
+
     vega_lite_config = {
         "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
         "description": "A scatterplot with a regression line",
@@ -88,7 +116,7 @@ def generate_vega_lite_json_EST(csv_file, classification_col, classification_col
                     "type": "text",
                     "align": "left",
                     "baseline": "bottom",
-                    "dx": 5, 
+                    "dx": 5,
                     "dy": -5
                 },
                 "data": {"values": [{"text": equation_text}]},
@@ -119,50 +147,69 @@ def generate_vega_lite_json_EST(csv_file, classification_col, classification_col
             },
         ]
     }
-    
+
     base_name = os.path.splitext(csv_file)[0]
     file_path = f"{sanitize_filename(classification_col_types)}_{name}.vega.json"
     with open(file_path, 'w') as file:
         json.dump(vega_lite_config, file, indent=4)
 
-def generate_vega_lite_json_Diffpercent(csv_file, classification_col, classification_col_types, x_field, y_field, fields, nominal_fields, name, include_all_data = False):
-    
-    tooltip_config = [{"field": field, "type": "nominal" if field in nominal_fields else "quantitative", "title": field} for field in fields]
+
+def generate_vega_lite_json_Diffpercent(
+        csv_file,
+        classification_col,
+        classification_col_types,
+        x_field,
+        y_field,
+        fields,
+        nominal_fields,
+        name,
+        include_all_data=False):
+
+    tooltip_config = [{"field": field,
+                       "type": "nominal" if field in nominal_fields else "quantitative",
+                       "title": field} for field in fields]
     vega_lite_config = {
         "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
         "description": "A scatterplot",
-        "data": {"url": csv_file},
-        "transform": [] if include_all_data else [{"filter": f"datum['{classification_col}'] == '{classification_col_types}'"}],
-         "layer": [
-                {
-            "mark": {
-                "type": "rule",
-                "color": "grey",
-                "strokeWidth": 3
-            },
-            "encoding": {
-                "y": {"datum": 0}
-            }
-        },    
-        {
-            "mark": "point",
-            "encoding": {
-                "x": {"field": x_field, "type": "quantitative"},
-                "y": {"field": y_field, "type": "quantitative"},
-                "tooltip": tooltip_config
-            }
-        }
-    ]
-}
+        "data": {
+            "url": csv_file},
+        "transform": [] if include_all_data else [
+            {
+                "filter": f"datum['{classification_col}'] == '{classification_col_types}'"}],
+        "layer": [
+            {
+                "mark": {
+                    "type": "rule",
+                            "color": "grey",
+                            "strokeWidth": 3},
+                "encoding": {
+                    "y": {
+                        "datum": 0}}},
+            {
+                "mark": "point",
+                "encoding": {
+                    "x": {
+                        "field": x_field,
+                        "type": "quantitative"},
+                    "y": {
+                        "field": y_field,
+                        "type": "quantitative"},
+                    "tooltip": tooltip_config}}]}
 
     base_name = os.path.splitext(csv_file)[0]
     file_path = f"{sanitize_filename(classification_col_types)}_{name}.vega.json"
     with open(file_path, 'w') as file:
         json.dump(vega_lite_config, file, indent=4)
 
-def generate_yaml_config(classification_col_type, name1, name2, include_all_data=False):
-    suffix = 'all' if include_all_data else sanitize_filename(classification_col_type)
-    
+
+def generate_yaml_config(
+        classification_col_type,
+        name1,
+        name2,
+        include_all_data=False):
+    suffix = 'all' if include_all_data else sanitize_filename(
+        classification_col_type)
+
     return [
         {
             "title": f"{suffix} - {name1}",
@@ -178,6 +225,7 @@ def generate_yaml_config(classification_col_type, name1, name2, include_all_data
         }
     ]
 
+
 yaml_config = {
     "header": {
         "tab": "Scatter plots",
@@ -186,4 +234,3 @@ yaml_config = {
     },
     "layout": {}
 }
-
--- road/validation_road.py
+++ road/validation_road.py
@@ -8,6 +8,7 @@ from sklearn.linear_model import LinearRegression
 from io import StringIO
 import geopandas as gpd
 from scatter import *
+from stats import *
 
 
 config = configparser.ConfigParser()
@@ -15,48 +16,39 @@ config.read('config.ini')
 
 # Extract the CHAMP input file names
 dbf_directory = config['DBF']['DBF_Directory']
-dbf_files = [dbf_file.strip() for dbf_file in config['DBF']['DBF_Files'].split(',')]
-dbf_column_names = [name.strip() for name in config['DBF']['DBF_Column_Names'].split(',')]
+dbf_files = [dbf_file.strip()
+             for dbf_file in config['DBF']['DBF_Files'].split(',')]
+dbf_column_names = [name.strip() for name in config['DBF']
+                    ['DBF_Column_Names'].split(',')]
 
 # Extract the observed file name and tab
 excel_file_path = config['OBSERVED FILE']['Excel_File_Path']
 sheet_name = config['OBSERVED FILE']['Sheet_Name']
-extra_columns = [col.strip() for col in config['OBSERVED FILE']['Excel_Extra_Columns'].split(',')]
+extra_columns = [col.strip() for col in config['OBSERVED FILE']
+                 ['Excel_Extra_Columns'].split(',')]
 obs_usecols = config['OBSERVED FILE']['Obs_usecols']
-time_periods = ['AM', 'MD', 'PM', 'EV', 'EA']
-
-#Part 1 
-# Scatter variables
-chosen_timeperiod = config['SCATTER INPUT']['Chosen_period']
-classification_col = config['SCATTER INPUT']['Classification_col']
-combined_df_cols = [col.strip() for col in config['SCATTER INPUT']['Combined_DF_Cols'].split(',')]
-
-# Est-Obs Plot
-fields1 = config['EST SCATTER PLOT']['Fields'].split(', ')
-nominal_fields1 = config['EST SCATTER PLOT']['NominalFields'].split(', ')
-x_field1 = config['EST SCATTER PLOT']['XField']
-y_field1 = config['EST SCATTER PLOT']['YField']
-name1 = config['EST SCATTER PLOT']['Name']
-
-# Percentile Plot
-fields2 = config['Perc SCATTER PLOT']['Fields'].split(', ')
-nominal_fields2 = config['Perc SCATTER PLOT']['NominalFields'].split(', ')
-x_field2 = config['Perc SCATTER PLOT']['XField']
-y_field2 = config['Perc SCATTER PLOT']['YField']
-name2 = config['Perc SCATTER PLOT']['Name']
-
-dashboard_num = config['YAML']['Dashboard_number']
-
 
 
 def handle_merged_header(excel_file, sheet_name, extra_columns):
-    df = pd.read_excel(excel_file, sheet_name=sheet_name, header=None, skiprows=1)
+    df = pd.read_excel(
+        excel_file,
+        sheet_name=sheet_name,
+        header=None,
+        skiprows=1)
     header = df.iloc[0]
     df = df[1:]
     df.columns = header
     return df[extra_columns]
 
-def filter_and_aggregate(excel_file, sheet_name, dbf_directory, dbf_files, column_names, time_periods, extra_columns):
+
+def filter_and_aggregate(
+        excel_file,
+        sheet_name,
+        dbf_directory,
+        dbf_files,
+        column_names,
+        time_periods,
+        extra_columns):
     excel_df = handle_merged_header(excel_file, sheet_name, extra_columns)
     base_df = excel_df.reset_index(drop=True)
 
@@ -64,12 +56,13 @@ def filter_and_aggregate(excel_file, sheet_name, dbf_directory, dbf_files, colum
         base_df[col] = 0
 
     for dbf_file, output_column in zip(dbf_files, time_periods):
-        dbf_file_path = f"{dbf_directory}\{dbf_file}"
+        dbf_file_path = f"{dbf_directory}\\{dbf_file}"
         dbf = Dbf5(dbf_file_path)
         dbf_df = dbf.to_dataframe()
         dbf_df = dbf_df[column_names]
 
-        dbf_dict = {(row['A'], row['B']): row for index, row in dbf_df.iterrows()}
+        dbf_dict = {(row['A'], row['B']): row for index,
+                    row in dbf_df.iterrows()}
 
         for index, row in base_df.iterrows():
             key = (row['A'], row['B'])
@@ -86,28 +79,116 @@ def filter_and_aggregate(excel_file, sheet_name, dbf_directory, dbf_files, colum
 
     return base_df
 
+
 # Read the Obs data and the CHAMP estimation data
-obs_df = pd.read_excel(excel_file_path, skiprows=1, sheet_name=sheet_name, usecols=obs_usecols)
-est_df = filter_and_aggregate(excel_file_path, sheet_name, dbf_directory, dbf_files, dbf_column_names, time_periods, extra_columns)
+time_periods = ['AM', 'MD', 'PM', 'EV', 'EA']
+obs_df = pd.read_excel(
+    excel_file_path,
+    skiprows=1,
+    sheet_name=sheet_name,
+    usecols=obs_usecols)
+est_df = filter_and_aggregate(
+    excel_file_path,
+    sheet_name,
+    dbf_directory,
+    dbf_files,
+    dbf_column_names,
+    time_periods,
+    extra_columns)
 
 
-# Part 1
-# calculate the metircs
-Select_time_period_loc_df, classification_col_types,file_name = compute_and_save_errors(est_df, obs_df, chosen_timeperiod, combined_df_cols, classification_col)
 
-# Generate the Scatter plots required fils
+# Part 1 - Scatter Plot
+# Scatter variables
+chosen_timeperiod = config['SCATTER INPUT']['Chosen_period']
+classification_col = config['SCATTER INPUT']['Classification_col']
+combined_df_cols = [col.strip() for col in config['SCATTER INPUT']
+                    ['Combined_DF_Cols'].split(',')]
+
+# Est-Obs Plot
+fields1 = config['EST SCATTER PLOT']['Fields'].split(', ')
+nominal_fields1 = config['EST SCATTER PLOT']['NominalFields'].split(', ')
+x_field1 = config['EST SCATTER PLOT']['XField']
+y_field1 = config['EST SCATTER PLOT']['YField']
+name1 = config['EST SCATTER PLOT']['Name']
+
+# Percentile Plot
+fields2 = config['PERCENT SCATTER PLOT']['Fields'].split(', ')
+nominal_fields2 = config['PERCENT SCATTER PLOT']['NominalFields'].split(', ')
+x_field2 = config['PERCENT SCATTER PLOT']['XField']
+y_field2 = config['PERCENT SCATTER PLOT']['YField']
+name2 = config['PERCENT SCATTER PLOT']['Name']
+
+# Dashborad num
+dashboard_num = config['YAML1']['Dashboard_number']
+
+
+# calculate the metrics
+Select_time_period_loc_df, classification_col_types, file_name = compute_and_save_errors(
+    est_df, obs_df, chosen_timeperiod, combined_df_cols, classification_col)
+
+# Generate the Scatter plots required files
 row_count = 1
 for types in classification_col_types:
-    generate_vega_lite_json_EST(file_name, classification_col,types, x_field1, y_field1, fields1, nominal_fields1,name1)
-    generate_vega_lite_json_Diffpercent(file_name, classification_col, types, x_field2, y_field2, fields2, nominal_fields2,name2)
+    generate_vega_lite_json_EST(
+        file_name,
+        classification_col,
+        types,
+        x_field1,
+        y_field1,
+        fields1,
+        nominal_fields1,
+        name1)
+    generate_vega_lite_json_Diffpercent(
+        file_name,
+        classification_col,
+        types,
+        x_field2,
+        y_field2,
+        fields2,
+        nominal_fields2,
+        name2)
 
-    yaml_config['layout'][f'row{row_count}'] = generate_yaml_config(types, name1, name2)
+    yaml_config['layout'][f'row{row_count}'] = generate_yaml_config(
+        types, name1, name2)
     row_count += 1
 
-generate_vega_lite_json_EST(file_name,classification_col, 'all', x_field1, y_field1, fields1, nominal_fields1, name1, include_all_data=True)
-generate_vega_lite_json_Diffpercent(file_name, classification_col, 'all', x_field2, y_field2, fields2, nominal_fields2, name2, include_all_data=True)
+generate_vega_lite_json_EST(
+    file_name,
+    classification_col,
+    'all',
+    x_field1,
+    y_field1,
+    fields1,
+    nominal_fields1,
+    name1,
+    include_all_data=True)
+generate_vega_lite_json_Diffpercent(
+    file_name,
+    classification_col,
+    'all',
+    x_field2,
+    y_field2,
+    fields2,
+    nominal_fields2,
+    name2,
+    include_all_data=True)
 
-yaml_config['layout'][f'row{row_count}'] = generate_yaml_config('all', name1, name2, include_all_data=True)
+yaml_config['layout'][f'row{row_count}'] = generate_yaml_config(
+    'all', name1, name2, include_all_data=True)
 yaml_file_path = f"dashboard{dashboard_num}-Scatter.yaml"
 with open(yaml_file_path, 'w') as file:
     yaml.dump(yaml_config, file, default_flow_style=False)
+
+
+# Part 2 - Validation Stats
+# the
+combined_df_cols = [col.strip() for col in config['STATS INPUT']
+                    ['Combined_DF_Cols'].split(',')]
+group_vars = ['Observed Volume', 'Loc Type', 'AT Grp', 'FT Grp']
+times = ['Daily', 'AM', 'MD', 'PM', 'EV', 'EA']
+
+
+time_period_dfs = prepare_time_period_dfs(
+    est_df, obs_df, times, combined_df_cols)
+generate_and_save_tables(time_period_dfs, group_vars)
